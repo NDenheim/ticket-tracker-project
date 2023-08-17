985e156d13783223e957d5c2074666cd92d5e323
NDenheim <nicole.denheim@hotmail.co.uk>
2023-08-17 19:37:16 +0100

    Edited member data and added routes to other pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { FormEvent, useState } from "react";
 import "./App.scss";
-import FilterBar from "./components/Filter/Filter";
-import DropdownBox from "./components/Dropdown/Dropdown";
 import TrackerTiles from "./components/TrackerTile/TrackerTile";
-import team from "./data/team";
+import Nav from "./components/Nav/Nav";
 import TeamMember from "./types/Member";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Tickets from "./containers/Tickets/Tickets";
+import Home from "./containers/Home/Home";
+import Profile from "./containers/Profile/Profile";
+import team from "./data/team";
 
 export const handleRender = (team: TeamMember[]) => {
   const members = team.map((member: any) => {
@@ -18,42 +21,17 @@ export const handleRender = (team: TeamMember[]) => {
 };
 
 const App = () => {
-  const [filter, setFilter] = useState<string>("");
-  const [dropdown, setDropdown] = useState<string>("");
-
-  const handleFilterByName = (event: FormEvent<HTMLInputElement>) => {
-    const userInput = event.currentTarget.value.toLowerCase();
-    setFilter(userInput);
-  };
-
-  let teamFilteredByName = team.filter((member) => {
-    return member.name.toLowerCase().includes(filter);
-  });
-
-  const handleFilterByRole = (event: FormEvent<HTMLSelectElement>) => {
-    const userInput = event.currentTarget.value;
-    setDropdown(userInput);
-  };
-
-  let teamFilteredByRole = teamFilteredByName.filter((member) => {
-    if (dropdown == "") {
-      return team;
-    } else {
-      return member.role == dropdown;
-    }
-  });
-
-  let ticketTrackers = handleRender(teamFilteredByRole);
-
   return (
-    <div className="app">
-      <h3 className="heading">Ticket Tracker</h3>
-      <div className="filters">
-        <FilterBar handleFilterByName={handleFilterByName} />
-        <DropdownBox handleFilterByRole={handleFilterByRole} />
+    <BrowserRouter>
+      <div className="app">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tickets" element={<Tickets />} />
+          <Route path="/profile" element={<Profile members={team} />} />
+        </Routes>
       </div>
-      <section className="all-tiles">{ticketTrackers}</section>
-    </div>
+    </BrowserRouter>
   );
 };
 
--- src/types/Member.ts
+++ src/types/Member.ts
@@ -2,6 +2,12 @@ type TeamMember = {
   id: number;
   name: string;
   role: string;
+  profile: {
+    experience: string;
+    department: string;
+    techstack: string[];
+    profilePicture: string;
+  };
 };
 
 export default TeamMember;
